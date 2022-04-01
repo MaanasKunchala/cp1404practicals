fa32bdd90621fe51b80d5fff9264c63ed1af1b82
MaanasKunchala <maanaslearn23@gmail.com>
2022-04-01 11:42:24 +0800

    Modify file

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,18 +2,31 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-
+import random
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if 0 <= score <= 100:
-    if score >= 90:
-        category = "Excellent"
-    elif score >= 50:
-        category = "Pass"
-    else:
-        category = "Bad"
-else:
-    category = "Invalid Value"
-print(category)
 
+def main():
+    score = float(input("Enter score: "))
+    category = get_category(score)
+    print(category)
+    random_score = random.randint(0, 100)
+    random_category = get_category(random_score)
+    print(f"random score = {random_score}")
+    print(random_category)
+
+
+def get_category(score):
+    if 0 <= score <= 100:
+        if score >= 90:
+            category = "Excellent"
+        elif score >= 50:
+            category = "Pass"
+        else:
+            category = "Bad"
+    else:
+        category = "Invalid Value"
+    return category
+
+
+main()
