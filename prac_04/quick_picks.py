5618956011d224eb5cf05d490ec0436bdf84f7cb
MaanasKunchala <maanaslearn23@gmail.com>
2022-04-08 11:45:09 +0800

    Update ""Quick Pick" Lottery Ticket Generator"

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,12 +1,16 @@
 import random
+NUMBERS_EACH_PICK = 6
+MIN_RANDOM_NUMBER = 1
+MAX_RANDOM_NUMBER = 45
+
 
 quick_picks = int(input("How many quick picks? "))
 for i in range(0, quick_picks):
     random_numbers = []
-    for number in range(0, 6):
-        random_number = random.randint(1, 45)
+    for number in range(0, NUMBERS_EACH_PICK):
+        random_number = random.randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
         while random_number in random_numbers:
-            random_number = random.randint(1, 45)
+            random_number = random.randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
         random_numbers.append(random_number)
     random_numbers.sort()
     for number in random_numbers:
