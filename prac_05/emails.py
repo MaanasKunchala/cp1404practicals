85642ab6e9dfc391f6302e516f6a6967d70cae3e
MaanasKunchala <maanaslearn23@gmail.com>
2022-04-22 11:39:53 +0800

    Complete emails.py question

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,8 +1,20 @@
 def main():
     email_to_name = {}
     email = input("Email: ")
-    name = get_name(email)
-    yes_or_no = input(f"Is your name {name}? (Y/n) ")
+    while email != "":
+        name = get_name(email)
+        yes_or_no = input(f"Is your name {name}? (Y/n) ").lower()
+        if yes_or_no == "y" or yes_or_no == "yes" or yes_or_no == "":
+            email_to_name[email] = name
+        elif yes_or_no == "n" or yes_or_no == "no":
+            name = input("Name: ")
+            email_to_name[email] = name
+        else:
+            print("Invalid Input")
+        email = input("Email: ")
+    print()
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 
 def get_name(email):
