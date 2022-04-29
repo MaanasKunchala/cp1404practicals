a518983c88ebb01d8fc6f7b577c642a852c2cbe9
MaanasKunchala <maanaslearn23@gmail.com>
2022-04-29 10:55:13 +0800

    Add get_age method to Guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -6,3 +6,9 @@ class Guitar:
 
     def __str__(self):
         return f"{self.name} ({self.year}) : ${self.cost:,}"
+
+    def get_age(self):
+        age = 2022 - self.year
+        return age
+
+
