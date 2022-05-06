38fc24c7839b2d3bf66eda4b663fe0b5c6f5f417
MaanasKunchala <maanaslearn23@gmail.com>
2022-05-06 14:35:03 +0800

    Create DynamicLabels class and add __init__ method

---
+++ prac_07/dynamic_labels.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.app import Builder
+
+
+class DynamicLabels(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        names = ["Cat", "Dog", "Dinosaur"]
+
+
+
