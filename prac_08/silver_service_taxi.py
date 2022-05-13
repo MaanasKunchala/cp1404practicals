ec686b8da9127a435ea755394fc166e4bf702e38
MaanasKunchala <maanaslearn23@gmail.com>
2022-05-13 11:11:31 +0800

    Update initial extra_fares value and update __str__ method

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -5,11 +5,11 @@ class SilverServiceTaxi(Taxi):
     """Specialised version of a Taxi that includes fanciness and extra costs."""
     flagfall = 4.50
 
-    def __init__(self, name, fuel, fanciness):
+    def __init__(self, name="", fuel=0, fanciness=0.0):
         """Initialise a SilverServiceTaxi instance."""
         super().__init__(name, fuel)
         self.price_per_km = (Taxi.price_per_km * fanciness)
-        self.extra_fares = 0
+        self.extra_fares = 1
 
     def start_fare(self):
         """Begins a new fare."""
@@ -22,4 +22,4 @@ class SilverServiceTaxi(Taxi):
 
     def __str__(self):
         """Return a string like a Taxi but with flagfall."""
-        return "{} plus flagfall of {}".format(super().__str__(), self.flagfall
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
