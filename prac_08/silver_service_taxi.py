954e68094ef16f58e4336d2f735be907bdc1f2b4
MaanasKunchala <maanaslearn23@gmail.com>
2022-05-13 10:40:34 +0800

    Override start_fare() and get_fare() methods to add flagfall

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -8,4 +8,14 @@ class SilverServiceTaxi(Taxi):
     def __init__(self, name, fuel, fanciness):
         """Initialise a SilverServiceTaxi instance."""
         super().__init__(name, fuel)
-        self.price_per_km = Taxi.price_per_km * fanciness
+        self.price_per_km = (Taxi.price_per_km * fanciness)
+        self.extra_fares = 0
+
+    def start_fare(self):
+        """Begins a new fare."""
+        self.current_fare_distance = 0
+        self.extra_fares += 1
+
+    def get_fare(self):
+        """Return the price for the taxi trip along with flagfall."""
+        return (self.price_per_km * self.current_fare_distance) + (self.flagfall * self.extra_fares)
