930e833f15f4fad86e75e306f14baf8a9df7de65
MaanasKunchala <maanaslearn23@gmail.com>
2022-05-13 09:42:57 +0800

    Add UnreliableCar class and __init__ method

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,9 @@
+from prac_08.car import Car
+
+
+class UnreliableCar:
+    def __init__(self, name, fuel, reliability):
+        self.name = name
+        self.fuel = fuel
+        self.reliability = reliability
+
