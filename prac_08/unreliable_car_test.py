86bf22b5f95354b07920c7817837f6bf4cc932f3
MaanasKunchala <maanaslearn23@gmail.com>
2022-05-13 10:01:28 +0800

    Test drive() method in UnreliableCar class

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -1,6 +1,13 @@
 from prac_08.unreliable_car import UnreliableCar
 
 my_car = UnreliableCar("Lamborghini", 100, 40)
+
+# Testing - __init__
 print(f"my_car name - {my_car.name}")
 print(f"my_car fuel - {my_car.fuel}")
 print(f"my_car reliability - {my_car.reliability}")
+
+# Testing - drive()
+print(my_car)
+my_car.drive(100)
+print(my_car)
