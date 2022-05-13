8e33660cf2ef6530f30299377b37e2f5da21c825
MaanasKunchala <maanaslearn23@gmail.com>
2022-05-13 09:24:28 +0800

    Complete Test Taxi exercises

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,19 @@
+from prac_08.taxi import Taxi
+
+# 1.
+my_taxi = Taxi("Prius 1", 100, 1.23)
+
+# 2.
+my_taxi.drive(40)
+
+# 3.
+print(my_taxi)
+print(f"Fare: {my_taxi.get_fare()}")
+
+# 4.
+my_taxi.start_fare()
+my_taxi.drive(100)
+
+# 5.
+print(my_taxi)
+print(f"Fare: {my_taxi.get_fare()}")
