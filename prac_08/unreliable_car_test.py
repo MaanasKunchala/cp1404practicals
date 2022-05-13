61205d47518e1695b50ca42cefa0cab2246100f1
MaanasKunchala <maanaslearn23@gmail.com>
2022-05-13 09:43:18 +0800

    Test __init__ method in UnreliableCar class

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,6 @@
+from prac_08.unreliable_car import UnreliableCar
+
+my_car = UnreliableCar("Lamborghini", 100, 40)
+print(f"my_car name - {my_car.name}")
+print(f"my_car fuel - {my_car.fuel}")
+print(f"my_car reliability - {my_car.reliability}")
