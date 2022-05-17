a447fb8addaf97c33a8efe89983658f4be12860b
MaanasKunchala <maanaslearn23@gmail.com>
2022-05-17 19:19:56 +0800

    Fix code to get valid taxi choice and add code to drive taxi

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -17,9 +17,10 @@ def main():
                 print(f"{i} - {taxi}")
             try:
                 taxi_option = int(input("Choose taxi: "))
-                current_taxi = taxis[taxi_option]
                 if taxi_option not in range(0, 3):
                     print("Invalid taxi choice")
+                else:
+                    current_taxi = taxis[taxi_option]
             except ValueError:
                 print("Invalid input")
             print(f"Bill to date: ${bill:.2f}")
@@ -28,13 +29,23 @@ def main():
 
         elif option == "d":
 
-            drive_taxi()
+            if current_taxi is not None:
+                distance = float(input("Drive how far? "))
+                current_taxi.drive(distance)
+                print(f"Your {current_taxi.name} trip cost you ${current_taxi.get_fare():.2f}")
+                bill += current_taxi.get_fare()
 
+            else:
+                print("You need to choose a taxi before you can drive")
+
+            print(f"Bill to date: ${bill:.2f}")
             print("q)uit, c)hoose taxi, d)rive")
             option = input(">>> ").lower()
 
         else:
             print("Invalid option")
+
+            print(f"Bill to date: ${bill:.2f}")
             print("q)uit, c)hoose taxi, d)rive")
             option = input(">>> ").lower()
 
