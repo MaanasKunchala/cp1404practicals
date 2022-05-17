d10c48e324b20c2c1e86b8a178a38ef4f8dfe68e
MaanasKunchala <maanaslearn23@gmail.com>
2022-05-17 19:04:40 +0800

    Add code to get valid taxi choice

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -4,18 +4,35 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    bill = 0
+    current_taxi = None
     print("Let's drive!")
     print("q)uit, c)hoose taxi, d)rive")
     option = input(">>> ").lower()
     while option != "q":
         if option == "c":
-            choose_taxi()
+
+            print("Taxis available:")
+            for i, taxi in enumerate(taxis):
+                print(f"{i} - {taxi}")
+            try:
+                taxi_option = int(input("Choose taxi: "))
+                current_taxi = taxis[taxi_option]
+                if taxi_option not in range(0, 3):
+                    print("Invalid taxi choice")
+            except ValueError:
+                print("Invalid input")
+            print(f"Bill to date: ${bill:.2f}")
             print("q)uit, c)hoose taxi, d)rive")
             option = input(">>> ").lower()
-        if option == "d":
+
+        elif option == "d":
+
             drive_taxi()
+
             print("q)uit, c)hoose taxi, d)rive")
             option = input(">>> ").lower()
+
         else:
             print("Invalid option")
             print("q)uit, c)hoose taxi, d)rive")
